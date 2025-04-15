c1efd77c71d7745e3e0796d0fcd52046b5432d17
Constantino14A <constantino.14a@gmail.com>
2025-04-15 10:38:47 -0500

    commit-3: demo index

--- js/carousel.js
+++ js/carousel.js
@@ -21,15 +21,49 @@ const swiper2 = new Swiper('.swiper2', {
   direction: 'horizontal',
   loop: true,
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper2-pagination',
+    clickable: true,
+  },
+
   // Navigation arrows
   navigation: {
     nextEl: '.swiper2-next',
     prevEl: '.swiper2-prev',
   },
+});
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-    draggable: true,
+var swiper3 = new Swiper('.swiper3', {
+  loop: true,
+  /* loopFillGroupWithBlank: true, */
+  autoplay: {
+    delay: 5000,  
+    disableOnInteraction: false,
+  },
+
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  grid: {
+    rows: 1,
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    768: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+    1000: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+    }
+  },
+  spaceBetween: 5,
+  pagination: {
+    el: ".swiper3-pagination",
+    clickable: true,
   },
 });
